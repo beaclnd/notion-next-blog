856a1d3b6035ede58e9ea1000eb52617aa88c93e
jaytsang <beaclnd92@gmail.com>
2023-12-24 01:30:50 +0800

    feat: Hide the non Public post within the search results

--- pages/api/search-notion.ts
+++ pages/api/search-notion.ts
@@ -14,6 +14,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const results = await search(searchParams)
   console.log('>>> lambda search-notion', results)
 
+  // To remove the post which is not Public from the results
+  const collectionKey = Object.keys(results.recordMap.collection)[0]
+  const schema = results.recordMap.collection[collectionKey].value.schema
+  const keyOfPublicProperty = Object.keys(schema)
+    .find((key) => schema[key].name === 'Public')
+  const idsNotPublic = results.results.filter((item) => {
+    const properties = results.recordMap.block[(item as any).id]?.value?.properties
+    const isNotPublic = properties?.[keyOfPublicProperty]?.[0]?.[0] !== 'Yes'
+    return isNotPublic
+  }).map((item) => item.id)
+  results.results = results.results?.filter((item) => !idsNotPublic.includes(item.id))
+  results.recordMap.block = Object.keys(results.recordMap.block)
+    .filter((id) => !idsNotPublic.includes(id))
+    .reduce((block, id) => ({ ...block, [id]: results.recordMap.block[id] }), {})
+  results.total = results?.results?.length || 0
+
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=60, max-age=60, stale-while-revalidate=60'
